82763f0495fa996a5a54f9504d353ea8d5fe5edb
masato315 <masato.kino315@gmail.com>
2022-05-27 17:05:00 +0900

    createアクションをコントローラーに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,5 +6,13 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-  
+
+  def create
+    Tweet.create(tewwt_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
